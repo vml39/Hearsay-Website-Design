77466114922d01625e97f66ec7b656a3ffcd0ddf
Seokjoo Yoon <seokjooyoon@dhcp-mcfad-1794.redrover.cornell.edu>
2017-11-06 17:44:12 -0500

    your message

---
+++ HTML/posters.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Posters</title>
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+</head>
+
+<body>
+
+  <h1>Hearsay Didn't Win the Oscar</h1>
+  <h1>Spring 2016</h1>
+  <img src="images/didnt-win-the-oscar.png" alt="Didn't Win the Oscar">
+
+  <h1>Hearsay Off the Record & On Social Media</h1>
+  <h1>Fall 2016</h1>
+  <img src="images/social-media.jpg" alt="On Social Media">
+
+  <h1>Hearsay’s Super Sweet Sixteen</h1>
+  <h1>Spring 2016</h1>
+  <img src="images/sweet-sixteen.png" alt="Super Sweet Sixteen">
+
+  <h1>Hearsay Goes Bankrupt</h1>
+  <h1>Fall 2016</h1>
+  <img src="images/hearsay-declares-bankruptcy.jpg" alt="Hearsay Goes Bankrupt">
+
+  <h1>Cirque de Hearsay</h1>
+  <h1>Spring 2015</h1>
+  <img src="images/cirque-de-hearsay.jpg" alt="Cirque de Hearsay">
+
+  <h1>Off the Record</h1>
+  <h1>Fall 2014</h1>
+  <img src="images/off-the-record.jpg" alt="Off the Record">
+
+  <h1>Candy Land</h1>
+  <h1>Spring 2014</h1>
+  <img src="images/candy-land.png" alt="Candy Land">
+
+</body>
+</html>
---
+++ HTML/upcoming.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Upcoming Performances</title>
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+</head>
+
+<body>
+
+  <h1>Upcoming Performances</h1>
+
+  <p>The Inauguration of Cornell’s 14th President 8/25</p>
+  <p>Hosted by Cornell University</p>
+
+</body>
+</html>
