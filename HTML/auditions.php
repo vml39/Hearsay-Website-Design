29d6018cff3908c5cd9604714b931fed308ebcc0
Seokjoo Yoon <seokjooyoon@dhcp-mcfad-1794.redrover.cornell.edu>
2017-11-06 17:34:52 -0500

    alumni

---
+++ HTML/auditions.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Auditions</title>
+  <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
+</head>
+
+<body>
+
+  <h1>Auditions</h1>
+
+  <h1>Next Auditions: Fall 2017!</h1>
+
+  <p>August 28th from 4:30PM – 6:30PM in Lincoln 107</p>
+
+  <p>August 29th from 4:30PM – 6:30PM in Lincoln 124</p>
+
+  <p>August 30th from 8:15PM – 10:00PM in RPCC 220</p>
+
+  <p>August 31st from 4:30PM – 6:30PM in Lincoln 140</p>
+
+  <p>See you there!!</p>
+
+
+  <h1>The Audition Process</h1>
+
+  <h1>1. Vocal Exercises</h1>
+
+  <p>We’ll start off by asking you to sing a couple of scales and take you
+    through a chord matching exercise to help us determine your range.</p>
+
+  <h1>2. Solo</h1>
+
+  <p>Next, we will ask you to perform a short solo, just a verse and chorus
+    of a song of your choice. It’s helpful to choose a song we will be familiar
+    with and one that fits comfortably in your range.</p>
+
+  <h1>3. Callbacks</h1>
+
+  <p>Later that week, we will hold callbacks- the second round of our audition
+    process. Here, you will be able to interact with the whole group and
+    participate in lots of fun activities so we have the chance to get to know
+    you and you can get to know us! Next, you will learn a portion of one of
+    our songs based on your voice part. You will also be asked to sing a second
+    solo, preferably a different genre from your first to showcase your range.
+    It’s a lot of fun, so relax!</p>
+
+  <p>Don’t be nervous! The audition process is easy to follow and will be over
+    before you know it. Best of luck to you!</p>
+
+</body>
+
+</html>
