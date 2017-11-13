37a71396121797a4b403269dadee585079b516d7
Vivian Li <vml39@cornell.edu>
2017-11-13 18:13:32 -0500

    alumni.php

--- alumni.php
+++ alumni.php
@@ -15,26 +15,24 @@
 
 <body>
   <!-- <?php
-   include "includes/navigation.php";
-   ?> -->
+  include "includes/navigation.php";
+  ?> -->
 
-   <h1>Alumnae</h1>
-   <div id="alum">
-     <p>If you are an alumna of Hearsay, we would love to hear from you! Please
-       email hearsayacappella@gmail.com and let us know what you are up to.
-       We will also add you to our alumnae listserve to keep you up to date with
-       all things Hearsay.</p>
-   </div>
+  <h1>Alumnae</h1>
+  <p>If you are an alumna of Hearsay, we would love to hear from you! Please
+    email hearsayacappella@gmail.com and let us know what you are up to.
+    We will also add you to our alumnae listserve to keep you up to date with
+    all things Hearsay.</p>
 
-     <p>Sign up for our listserve!</p>
-     <span>Email: </span><input id="alum_email" name="email" type="email">
+    <p>Sign up for our listserve!</p>
+    <span>Email: </span><input id="alum_email" name="email" type="email">
 
-     <!-- need to validate email; include source! -->
-
+    <!-- need to validate email; include source! -->
+    
   </div>
 
-   <!-- <?php
-   include "includes/footer.php";
-   ?> -->
+  <!-- <?php
+  include "includes/footer.php";
+  ?> -->
 
 </body>
