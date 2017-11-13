0eb2e74961e845593de8ffe1683ad1e144209b9c
Vivian Li <vml39@cornell.edu>
2017-11-13 18:10:14 -0500

    spacing

--- contact.php
+++ contact.php
@@ -17,34 +17,38 @@
     include "includes/navigation.php";
     ?> -->
 
-    <h1>Contact Us</h1>
+    <div class="body_container">
 
-    <p>We would love to hear from you. If you would like to know where we are performing next, information about our semester concert, would like us to perform at your event, or anything else, just ask!</p>
+      <h1>Contact Us</h1>
 
-    <form id="contactForm" action="contact-response.php" method="post" novalidate>
-      <span>Name (required)</span><br/>
-      <input id="name" class="formInput" name="name" type="text" required>
-      <!-- this error message is hidden by default -->
-      <span class="errorContainer hiddenError" id="nameError">
-        <br/>Name is required.
-      </span>
+      <p>We would love to hear from you. If you would like to know where we are performing next, information about our semester concert, would like us to perform at your event, or anything else, just ask!</p>
 
-      <br/><span>Email (required)</span><br/>
-      <input id="email" class="formInput" name="email" type="email" required>
-      <!-- this error message is hidden by default -->
-      <span class="errorContainer hiddenError" id="emailError">
-        <br/>A valid email is required.
-      </span>
+      <form id="contactForm" action="contact-response.php" method="post" novalidate>
+        <span>Name (required)</span><br/>
+        <input id="name" class="formInput" name="name" type="text" required>
+        <!-- this error message is hidden by default -->
+        <span class="errorContainer hiddenError" id="nameError">
+          <br/>Name is required.
+        </span>
 
-      <br/><span>Message</span><br/>
-      <textarea id="comments" class="formInput" name="comments" required></textarea>
-      <!-- this error message is hidden by default -->
-      <span class="errorContainer hiddenError" id="messageError">
-        <br/>A message is required.
-      </span>
+        <br/><span>Email (required)</span><br/>
+        <input id="email" class="formInput" name="email" type="email" required>
+        <!-- this error message is hidden by default -->
+        <span class="errorContainer hiddenError" id="emailError">
+          <br/>A valid email is required.
+        </span>
 
-    <br/><button type="submit" class="submit">Submit</button>
-  </form>
+        <br/><span>Message</span><br/>
+        <textarea id="comments" class="formInput" name="comments" required></textarea>
+        <!-- this error message is hidden by default -->
+        <span class="errorContainer hiddenError" id="messageError">
+          <br/>A message is required.
+        </span>
+
+      <br/><button type="submit" class="submit">Submit</button>
+    </form>
+
+  </div>
 
 
     <!-- <?php
--- current_members.php
+++ current_members.php
@@ -17,45 +17,47 @@
    include "includes/navigation.php";
    ?> -->
 
-   <h1>Current Members</h1>
+   <div class="body_container">
 
-   <!-- display members in a grid fashion with three profiles per grid -->
-   <!-- tried to do a popup modal but still trying to figure it out -->
-   <div class="container">
-    <div class="first profile">
-      <!-- trigger the modal
-      <button id="modal_btn"> -->
+     <h1>Current Members</h1>
+
+     <!-- display members in a grid fashion with three profiles per grid -->
+     <!-- tried to do a popup modal but still trying to figure it out -->
+     <div class="container">
+      <div class="first profile">
+        <!-- trigger the modal
+        <button id="modal_btn"> -->
+          <img alt="" src="images/gray.jpg">
+        <!-- </button>
+        modal
+        <div id="modal">
+        modal content
+          <div id="modal_content">
+            <span class="close">&times;</span>
+            <p>some stuff that pops up</p>
+          </div>
+        </div> -->
+      </div>
+      <div class="second profile">
         <img alt="" src="images/gray.jpg">
-      <!-- </button>
-      modal
-      <div id="modal">
-      modal content
-        <div id="modal_content">
-          <span class="close">&times;</span>
-          <p>some stuff that pops up</p>
-        </div>
-      </div> -->
-    </div>
-    <div class="second profile">
-      <img alt="" src="images/gray.jpg">
-    </div>
-    <div class="third profile">
-      <img alt="" src="images/gray.jpg">
-    </div>
-   </div>
+      </div>
+      <div class="third profile">
+        <img alt="" src="images/gray.jpg">
+      </div>
+     </div>
 
-   <div class="container">
-    <div class="first profile">
-      <img alt="" src="images/gray.jpg">
-    </div>
-    <div class="second profile">
-      <img alt="" src="images/gray.jpg">
-    </div>
-    <div class="third profile">
-      <img alt="" src="images/gray.jpg">
+     <div class="container">
+      <div class="first profile">
+        <img alt="" src="images/gray.jpg">
+      </div>
+      <div class="second profile">
+        <img alt="" src="images/gray.jpg">
+      </div>
+      <div class="third profile">
+        <img alt="" src="images/gray.jpg">
+      </div>
     </div>
   </div>
-
 
    <!-- <?php
    include "includes/footer.php";
