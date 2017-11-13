95622c5a9234e4b2eedf395ac11bccbe8a57413f
Sarah Zhang <Sarah@dhcp-gs-7868.redrover.cornell.edu>
2017-11-13 17:48:59 -0500

    your message

--- index.php
+++ index.php
@@ -8,16 +8,19 @@
 </head>
 
 <body id="about">
-  <!-- <?php
+
+  <?php
    include "includes/navigation.php";
-   ?> -->
+   ?>
 
-   <!-- photo -->
-   <!-- line -->
-   <p>Hearsay A Cappella is one of Cornell University’s premier all-female a cappella groups. Founded in 2000 as an alternative group, we pride ourselves on an expansive repertoire spanning a wide variety of genres. Whatever your musical preference, Hearsay sings it! Our first album, Hear What?, debuted in 2007 and features songs by Oasis, No Doubt, and Billy Joel, just to name a few. In 2012 we released our second album, Opening Statement, which includes songs by Jefferson Airplane, Adele, Ingrid Michaelson, Lily Allen, and Phoenix. Stay tuned for our third album, which is in the works and will be released this year!</p>
+   <div class="body_container">
+     <!-- photo -->
+     <!-- line -->
+     <p>Hearsay A Cappella is one of Cornell University’s premier all-female a cappella groups. Founded in 2000 as an alternative group, we pride ourselves on an expansive repertoire spanning a wide variety of genres. Whatever your musical preference, Hearsay sings it! Our first album, Hear What?, debuted in 2007 and features songs by Oasis, No Doubt, and Billy Joel, just to name a few. In 2012 we released our second album, Opening Statement, which includes songs by Jefferson Airplane, Adele, Ingrid Michaelson, Lily Allen, and Phoenix. Stay tuned for our third album, which is in the works and will be released this year!</p>
+   </div>
 
-   <!-- <?php
+   <?php
    include "includes/footer.php";
-   ?> -->
+   ?>
 
 </body>
