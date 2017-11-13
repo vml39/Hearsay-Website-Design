0323f3b81742c8f4ed6e14bbab282120d749e29c
Sarah Zhang <Sarah@dhcp-gs-7868.redrover.cornell.edu>
2017-11-13 17:02:39 -0500

    nav and footer

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+  <ul>
+    <li><a href=""> <i class="fa fa-instagram fa-1x"></i> </a></li>
+    <li><a href=""> <i class="fa fa-facebook-square fa-1x"></i> </a></li>
+    <li><a href=""><i class="fa fa-youtube-square fa-1x"></i> </a></li>
+    <li><a href=""><i class="fa fa-spotify fa-1x"></i></a></li>
+  </ul>
+</div>
+</html>
