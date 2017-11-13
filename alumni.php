1625b4b6bd181ca695577e660e93ea596c819f68
Vivian Li <vml39@cornell.edu>
2017-11-13 16:00:12 -0500

    tried to make a modal but failed

--- alumni.php
+++ alumni.php
@@ -5,6 +5,10 @@
   <meta charset="utf-8">
   <title>Hearsay A Cappella</title>
   <link rel="stylesheet" type ="text/css" href="styles/all.css" media="all"/>
+  <link rel="stylesheet" type ="text/css" href="styles/all.css" media="all"/>
+  <link rel="stylesheet" type ="text/css" href="styles/members.css" media="all"/>
+  <script src="scripts/jquery-3.2.1.min.js" type="text/javascript"></script>
+  <script src="scripts/members.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -12,7 +16,16 @@
    include "includes/navigation.php";
    ?> -->
 
-  
+   <h1>Alumnae</h1>
+   <div id="alum">
+     <p>If you are an alumna of Hearsay, we would love to hear from you! Please email hearsayacappella@gmail.com and let us know what you are up to. We will also add you to our alumnae listserve to keep you up to date with all things Hearsay.</p>
+   </div>
+
+   <p>Sign up for our listserve!</p>
+   Email: <input id="alum_email" name="email" type="email">
+
+   <!-- need to validate email -->
+
    <!-- <?php
    include "includes/footer.php";
    ?> -->
--- current_members.php
+++ current_members.php
@@ -5,6 +5,9 @@
   <meta charset="utf-8">
   <title>Hearsay A Cappella</title>
   <link rel="stylesheet" type ="text/css" href="styles/all.css" media="all"/>
+  <link rel="stylesheet" type ="text/css" href="styles/members.css" media="all"/>
+  <script src="scripts/jquery-3.2.1.min.js" type="text/javascript"></script>
+  <script src="scripts/members.js" type="text/javascript"></script>
 </head>
 
 <body>
@@ -12,13 +15,23 @@
    include "includes/navigation.php";
    ?> -->
 
+   <h1>Current Members</h1>
+
    <!-- display members in a grid fashion with three profiles per grid -->
    <div class="container">
     <div class="first profile">
-      <button class="modal">
+      <!-- trigger the modal -->
+      <button id="modal_btn">
         <img alt="" src="images/gray.jpg">
       </button>
-      <p class="modalcontent" id="name1">some stuff that pops up</p>
+      <!-- modal -->
+      <div id="modal">
+      <!-- modal content -->
+        <div id="modal_content">
+          <span class="close">&times;</span>
+          <p>some stuff that pops up</p>
+        </div>
+      </div>
     </div>
     <div class="second profile">
       <img alt="" src="images/gray.jpg">
@@ -38,7 +51,7 @@
     <div class="third profile">
       <img alt="" src="images/gray.jpg">
     </div>
-   </div>
+  </div>
 
 
    <!-- <?php
