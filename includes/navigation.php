22e9a0a5839f2a7188a8be30575136e2b47353ca
Sarah Zhang <Sarah@dhcp-gs-205.redrover.cornell.edu>
2017-11-14 16:16:03 -0500

    nav and footer

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,18 +1,13 @@
-
-<div class="nav1">
-  <ul>
-    <li><a href="index.php">HOME</a></li>
-    <li><a href="members.php">MEMBERS</a></li>
-    <li><a href="auditions.php">AUDITION</a></li>
-  </ul>
-</div>
-
-  <div class="logo">
+<div class="navlogo">
       <a href="index.php"><img src='images/logo.png' alt="logo"></a>
   </div>
 
-<div class="nav2">
+<div class="nav">
   <ul>
+    <ul>
+      <li><a href="index.php">HOME</a></li>
+      <li><a href="current_members.php">MEMBERS</a></li>
+      <li><a href="auditions.php">AUDITION</a></li>
     <li><a href="repertoire.php">MUSIC</a></li>
       <li><a href="faqs.php">FAQ</a></li>
     <li><a href="contact.php">CONTACT</a></li>
--- includes/navigation_index.php
+++ includes/navigation_index.php
@@ -1,26 +1,19 @@
-<head>
-<!---Font Awesome Icons used are sourced from https://www.w3schools.com/icons/default.asp--->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-
-<div class="nav1">
+<div class="nav-index">
   <ul>
     <li><a href="index.php">HOME</a></li>
-    <li><a href="members.php">MEMBERS</a></li>
+    <li><a href="current_members.php">MEMBERS</a></li>
     <li><a href="auditions.php">AUDITION</a></li>
-  </ul>
-</div>
-
-  <div class="logo">
-      <a href="index.php"><img src='images/logo.png' alt="logo"></a>
-  </div>
-
-<div class="nav2">
-  <ul>
-    <li><a href="">MUSIC</a></li>
-      <li><a href="faqs.php">FAQ</a></li>
+    <li><a href="repertoire.php">MUSIC</a></li>
+    <li><a href="faqs.php">FAQ</a></li>
     <li><a href="contact.php">CONTACT</a></li>
   </ul>
 </div>
 
-<img src="images/frontpage-cover.jpg" alt="frontpage-cover">
+<div class="container">
+  <div class="frontcover">
+    <img src="images/frontpage-cover.jpg" alt="frontpage-cover">
+  </div>
+  <div class="logo-index">
+      <a href="index.php"><img src='images/logo.png' alt="logo"></a>
+  </div>
+</div>
