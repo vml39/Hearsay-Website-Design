c30b4be072448599bf3f637604d37575e0487687
Vivian Li <vml39@cornell.edu>
2017-11-14 12:14:01 -0500

    added all the members

--- current_members.php
+++ current_members.php
@@ -19,9 +19,7 @@
 
    <div class="body_container">
 
-     <h1>Current Members</h1>
-
-     <!-- other members will be added on later -->
+     <h1>Meet the Hearsayers</h1>
 
      <!-- display members in a grid fashion with three profiles per grid -->
      <!-- tried to do a popup modal but still trying to figure it out, using https://www.w3schools.com/howto/howto_css_modals.asp -->
@@ -46,64 +44,79 @@
         <p>Veronica Dickson, Co-Social Chair</p>
       </div>
       <div class="third profile">
-        <img alt="" src="images/gray.jpg">
+        <img alt="" src="images/members/katie.jpeg">
+        <p>Katie O’Neill, Musical Director</p>
       </div>
      </div>
 
      <div class="container">
       <div class="first profile">
-        <img alt="" src="images/gray.jpg">
+        <img alt="" src="images/members/chrissy.jpg">
+        <p>Chrissy Chen, Co-Social Chair</p>
       </div>
       <div class="second profile">
-        <img alt="" src="images/gray.jpg">
+        <img alt="" src="images/members/samantha.jpg">
+        <p>Samantha Li, President</p>
       </div>
       <div class="third profile">
-        <img alt="" src="images/gray.jpg">
+        <img alt="" src="images/members/anna.jpg">
+        <p>Anna Bores, Assistant Musical Director</p>
       </div>
     </div>
 
     <div class="container">
      <div class="first profile">
-       <img alt="" src="images/gray.jpg">
+       <img alt="" src="images/members/lydia.png">
+       <p>Lydia Clemente</p>
      </div>
      <div class="second profile">
-       <img alt="" src="images/gray.jpg">
+       <img alt="" src="images/members/audrey.jpg">
+       <p>Audrey Kennedy, Vice President</p>
      </div>
      <div class="third profile">
-       <img alt="" src="images/gray.jpg">
+       <img alt="" src="images/members/danica.jpg">
+       <p>Danica Lee, Business Manager</p>
      </div>
    </div>
 
    <div class="container">
     <div class="first profile">
-      <img alt="" src="images/gray.jpg">
+      <img alt="" src="images/members/quyen.jpg">
+      <p>Quyen Nguyen, Marketing Chair</p>
     </div>
     <div class="second profile">
-      <img alt="" src="images/gray.jpg">
+      <img alt="" src="images/members/lily.jpg">
+      <p>Lily Pagan</p>
     </div>
     <div class="third profile">
-      <img alt="" src="images/gray.jpg">
+      <img alt="" src="images/members/mihoko.jpg">
+      <p>Mihoko Sakanaka</p>
     </div>
   </div>
 
   <div class="container">
    <div class="first profile">
-     <img alt="" src="images/gray.jpg">
+     <img alt="" src="images/members/sam_ng.jpg">
+     <p>Samantha Ng, Social Media Chair</p>
    </div>
    <div class="second profile">
-     <img alt="" src="images/gray.jpg">
+     <img alt="" src="images/members/vanessa.jpg">
+     <p>Vanessa Okoyeh</p>
    </div>
    <div class="third profile">
-     <img alt="" src="images/gray.jpg">
+     <img alt="" src="images/members/chase.jpg">
+     <p>Chase Kauder</p>
    </div>
   </div>
 
   <div class="container">
    <div class="first profile">
-     <img alt="" src="images/gray.jpg">
+     <img alt="" src="images/members/jillian.jpg">
+     <p>Jillian Berkowitz</p>
    </div>
    <div class="second profile">
-     <img alt="" src="images/gray.jpg">
+     <img alt="" src="images/members/melissa.jpg">
+     <p>Melissa Gao</p>
    </div>
   </div>
 
