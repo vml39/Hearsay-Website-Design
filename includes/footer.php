fbea77c2d4de3096a386696d136d2a49e883ba7f
Vivian Li <vml39@cornell.edu>
2017-11-20 17:08:36 -0500

    footer links

--- includes/footer.php
+++ includes/footer.php
@@ -5,10 +5,10 @@
 
 <div class="footer">
   <ul>
-    <li><a href=""> <i class="fa fa-instagram fa-1x"></i> </a></li>
-    <li><a href=""> <i class="fa fa-facebook-square fa-1x"></i> </a></li>
-    <li><a href=""><i class="fa fa-youtube-square fa-1x"></i> </a></li>
-    <li><a href=""><i class="fa fa-spotify fa-1x"></i></a></li>
+    <li><a href="https://www.instagram.com/hearsayacappella/?hl=en"> <i class="fa fa-instagram fa-1x"></i> </a></li>
+    <li><a href="https://www.facebook.com/HearsayACappella/"> <i class="fa fa-facebook-square fa-1x"></i> </a></li>
+    <li><a href="https://www.youtube.com/user/HearsayACappella"><i class="fa fa-youtube-square fa-1x"></i> </a></li>
+    <li><a href=""><i class="fa fa-spotify fa-1x"></i></a> Coming Out Soon!</li>
   </ul>
 </div>
 </html>
