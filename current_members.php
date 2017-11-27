8fde136b54a471d2067b510f3b9d472f784c5111
Vivian Li <vml39@cornell.edu>
2017-11-26 22:51:42 -0500

    all member info

--- current_members.php
+++ current_members.php
@@ -48,80 +48,150 @@
        </div>
        <div class="member" id="katie_info">
          <h1>Katie O’Neill</h1>
-         <p class="info">Year: 2018<br/>Major: Civil Engineering<br/>Joined: Fall 2014<br/>Voice Part: Soprano 1<br/>Audition Song: Last Love Song by ZZ Ward<br/>Campus Involvement: Concrete Canoe Team, American Society for Civil Engineers. <br/>Fun Fact: Katie plays six different instruments.</p>
+         <p class="info">Year: 2018<br/>Major: Civil Engineering<br/>Joined: Fall 2014<br/>Voice Part: Soprano I<br/>Audition Song: Last Love Song by ZZ Ward<br/>Campus Involvement: Concrete Canoe Team, American Society for Civil Engineers.<br/>Fun Fact: Katie plays six different instruments.</p>
        </div>
       </div>
 
      <div class="container">
       <div class="first profile">
-        <img alt="" src="images/members/chrissy.jpg">
+        <img id="chrissy" alt="" src="images/members/chrissy.jpg">
         <p class="name">Chrissy Chen, Co-Social Chair</p>
       </div>
       <div class="second profile">
-        <img alt="" src="images/members/samantha.jpg">
+        <img id="samli" alt="" src="images/members/samantha.jpg">
         <p class="name">Samantha Li, President</p>
       </div>
       <div class="third profile">
-        <img alt="" src="images/members/anna.jpg">
+        <img id="anna" alt="" src="images/members/anna.jpg">
         <p class="name">Anna Bores, Assistant Musical Director</p>
       </div>
     </div>
 
+    <div class="member_info">
+      <div class="member" id="chrissy_info">
+        <h1>Chrissy Chen</h1>
+        <p class="info">Year: 2018<br/>Major: Food Science<br/>Joined: Spring 2015<br/>Voice Part: Alto<br/>Audition Song: Pretty Hurts by Beyoncé<br/>Campus Involvement: Food Science Club, ChocTech Club, Product Development Competitions<br/>Fun Fact: Chrissy just learned how to do a cartwheel this semester and she’s proud of it. </p>
+      </div>
+      <div class="member" id="samli_info">
+        <h1>Samantha Li</h1>
+        <p class="info">Year: 2018<br/>Major: Animal Science<br/>Joined: Spring 2017<br/>Voice Part: Bass<br/>Audition Song: All I Ask by Adele<br/>Campus Involvement: Pre-Vet Society, Block and Bridle, Creme de Cornell, Swoopers and Bashers<br/>Fun Fact: Sam has three tattoos that she got in the span of 6 months.</p>
+      </div>
+      <div class="member" id="anna_info">
+        <h1>Anna Bores</h1>
+        <p class="info">Year: 2019<br/>Major: ILR with Music minor<br/>Joined: Fall 2016<br/>Voice Part: Soprano II<br/>Audition Song: I See Fire by Ed Sheeran<br/>Campus Involvement: Chorus, Cornell Radio DJ<br/>Fun Fact: I named my fish Professor Blubber Nuggets and my dad insists on calling him Goldie (but we all know which name is better).</p>
+      </div>
+     </div>
+
     <div class="container">
      <div class="first profile">
-       <img alt="" src="images/members/lydia.png">
+       <img id="lydia" alt="" src="images/members/lydia.png">
        <p class="name">Lydia Clemente</p>
      </div>
      <div class="second profile">
-       <img alt="" src="images/members/audrey.jpg">
+       <img id="audrey" alt="" src="images/members/audrey.jpg">
        <p class="name">Audrey Kennedy, Vice President</p>
      </div>
      <div class="third profile">
-       <img alt="" src="images/members/danica.jpg">
+       <img id="danica" alt="" src="images/members/danica.jpg">
        <p class="name">Danica Lee, Business Manager</p>
      </div>
    </div>
 
+   <div class="member_info">
+     <div class="member" id="lydia_info">
+       <h1>Lydia Clemente</h1>
+       <p class="info">Year: 2020<br/>Major: Communication<br/>Joined: Fall 2016<br/>Voice Part: Alto<br/>Audition Song: Gravity by Sara Bareilles<br/>Campus Involvement: Outdoor Odyssey<br/>Fun Fact: Lydia has 32 cousins on one side of her family.</p>
+     </div>
+     <div class="member" id="audrey_info">
+       <h1>Audrey Kennedy</h1>
+       <p class="info">Year: 2020<br/>Major: Biochemistry<br/>Joined: Fall 2016<br/>Voice Part: Bass<br/>Audition Song: Love You Like a Lovesong by Selena Gomez<br/>Campus Involvement: <br/>Fun Fact: Audrey has practiced Kung Fu for 8 years and has earned her Brown Sash.</p>
+     </div>
+     <div class="member" id="danica_info">
+       <h1>Danica Lee</h1>
+       <p class="info">Year: 2020<br/>Major: Biology<br/>Joined: Fall 2016<br/>Voice Part: Bass<br/>Audition Song: If I Ain’t Got You by Alicia Keys<br/>Campus Involvement: Member of Society for Natural Resources Conservation; CURB’s Peer Mentorship Program; Member of Woman of Color Coalition<br/>Fun Fact: Danica is currently trying her best to keep three plants alive: Harold the Snake Plant, Harriet the Air Plant, and Steve the Fuzzy Plant (RIP Buddy).</p>
+     </div>
+    </div>
+
    <div class="container">
     <div class="first profile">
-      <img alt="" src="images/members/quyen.jpg">
+      <img id="quyen" alt="" src="images/members/quyen.jpg">
       <p class="name">Quyen Nguyen, Marketing Chair</p>
     </div>
     <div class="second profile">
-      <img alt="" src="images/members/lily.jpg">
+      <img id="lily" alt="" src="images/members/lily.jpg">
       <p class="name">Lily Pagan</p>
     </div>
     <div class="third profile">
-      <img alt="" src="images/members/mihoko.jpg">
+      <img id="mihoko" alt="" src="images/members/mihoko.jpg">
       <p class="name">Mihoko Sakanaka</p>
     </div>
   </div>
 
+  <div class="member_info">
+    <div class="member" id="quyen_info">
+      <h1>Quyen Nguyen</h1>
+      <p class="info">Year: 2020<br/>Major: Hotel Management<br/>Joined: Fall 2016<br/>Voice Part: Bass<br/>Audition Song: Tears Dry On Their Own by Amy Winehouse<br/>Campus Involvement: Hotel Ezra Cornell, Statler Hotel<br/>Fun Fact: Quyen is a polyglot; her first language is Vietnamese, and her second language is French.</p>
+    </div>
+    <div class="member" id="lily_info">
+      <h1>Lily Pagan</h1>
+      <p class="info">Year: 2020<br/>Major: Information Science<br/>Joined: Fall 2016<br/>Voice Part: Bass<br/>Audition Song: Hallelujah by Alicia Keys<br/>Campus Involvement: Guiding Eyes<br/>Fun Fact: Lily skipped 3rd grade.</p>
+    </div>
+    <div class="member" id="mihoko_info">
+      <h1>Mihoko Sakanaka</h1>
+      <p class="info">Year: 2020<br/>Major: Chemical Engineering<br/>Joined: Fall 2016<br/>Voice Part: Soprano I<br/>Audition Song: Fly Fly Away from Catch Me If You Can<br/>Campus Involvement: Engineers without Borders, On Tap Dance Troupe<br/>Fun Fact: Mihoko once ate an entire bag of dried mangoes – and would definitely do it again.</p>
+    </div>
+   </div>
+
   <div class="container">
    <div class="first profile">
-     <img alt="" src="images/members/sam_ng.jpg">
+     <img id="samng" alt="" src="images/members/sam_ng.jpg">
      <p class="name">Samantha Ng, Social Media Chair</p>
    </div>
    <div class="second profile">
-     <img alt="" src="images/members/vanessa.jpg">
+     <img id="vanessa" alt="" src="images/members/vanessa.jpg">
      <p class="name">Vanessa Okoyeh</p>
    </div>
    <div class="third profile">
-     <img alt="" src="images/members/chase.jpg">
+     <img id="chase" alt="" src="images/members/chase.jpg">
      <p class="name">Chase Kauder</p>
    </div>
   </div>
 
+  <div class="member_info">
+    <div class="member" id="samng_info">
+      <h1>Samantha Ng</h1>
+      <p class="info">Year: 2020<br/>Major: Sociology, PAM Minor<br/>Joined: Spring 2017<br/>Voice Part: Alto<br/>Audition Song: She Will Be Loved By Maroon 5<br/>Campus Involvement: Kappa Delta, FBLA – Phi Beta Lambda<br/>Fun Fact: Sam loves spicy things — that includes food and people!!</p>
+    </div>
+    <div class="member" id="vanessa_info">
+      <h1>Vanessa Okoyeh</h1>
+      <p class="info">Year: 2019<br/>Major: Industrial Labor Relations<br/>Joined: Spring 2017<br/>Voice Part: Alto<br/>Audition Song: Almost Like Being in Love from Bridgadoon<br/>Campus Involvement: PBNQ Improv, Cornell Public Service Center, Quiz Bowl<br/>Fun Fact: Vanessa’s mom’s favorite color is wine red; she does not drink.</p>
+    </div>
+    <div class="member" id="chase_info">
+      <h1>Chase Kauder</h1>
+      <p class="info">Year: 2021<br/>Major: Applied Economics & Management<br/>Joined: Fall 2017<br/>Voice Part: Mezzo<br/>Audition Song: Hallelujah, I Love Her So By Ray Charles<br/>Campus Involvement: After Six<br/>Fun Fact: Chase loves Pinterest and hopes to one day successfully conquer a Pinterest craft project.</p>
+    </div>
+   </div>
+
   <div class="container">
    <div class="first profile">
-     <img alt="" src="images/members/jillian.jpg">
+     <img id="jillian" alt="" src="images/members/jillian.jpg">
      <p class="name">Jillian Berkowitz</p>
    </div>
    <div class="second profile">
-     <img alt="" src="images/members/melissa.jpg">
+     <img id="melissa" alt="" src="images/members/melissa.jpg">
      <p class="name">Melissa Gao</p>
    </div>
   </div>
+
+  <div class="member_info">
+    <div class="member" id="jillian_info">
+      <h1>Jillian Berkowitz</h1>
+      <p class="info">Year: 2019<br/>Major: Government<br/>Joined: Fall 2017<br/>Voice Part: Alto<br/>Audition Song: Feeling Good by Michael Bublé<br/>Campus Involvement: Alpha Phi Omega, Cornell Democrats<br/>Fun Fact: Jillian was baptized and bat mitzvah’d — but that’s okay because religion is a social construct!</p>
+    </div>
+    <div class="member" id="melissa_info">
+      <h1>Melissa Gao</h1>
+      <p class="info">Year: 2021<br/>Major: Industrial Labor Relations<br/>Joined: Fall 2017<br/>Voice Part: Alto<br/>Audition Song: In My Life by The Beatles<br/>Campus Involvement: Chorus, CSA, Risley Dining<br/>Fun Fact: Melissa can do the Stitch voice really well!</p>
+    </div>
 
   </div>
 
--- scripts/members.js
+++ scripts/members.js
@@ -2,15 +2,85 @@ $(document).ready(function() {
 
 // how to correspond people to their information profiles so that I dont have to do this
 // can I mix php with jquery??
+  // row 1
   $("#belan").click( function(){
-      $("#belan_info").toggle();
-      // also need to hide the other profiles how to only have one profile open at the same time
+      $(".member").hide();
+      $("#belan_info").show();
+      // can toggle but need to figure out how to close it
   });
   $("#veronica").click( function(){
-      $("#veronica_info").toggle();
+    $(".member").hide();
+    $("#veronica_info").show();
   });
   $("#katie").click( function(){
-      $("#katie_info").toggle();
+    $(".member").hide();
+    $("#katie_info").show();
+  });
+
+  // row 2
+  $("#chrissy").click( function(){
+    $(".member").hide();
+    $("#chrissy_info").show();
+  });
+  $("#samli").click( function(){
+    $(".member").hide();
+    $("#samli_info").show();
+  });
+  $("#anna").click( function(){
+    $(".member").hide();
+    $("#anna_info").show();
+  });
+
+  // row 3
+  $("#lydia").click( function(){
+    $(".member").hide();
+    $("#lydia_info").show();
+  });
+  $("#audrey").click( function(){
+    $(".member").hide();
+    $("#audrey_info").show();
+  });
+  $("#danica").click( function(){
+    $(".member").hide();
+    $("#danica_info").show();
+  });
+
+  // row 4
+  $("#quyen").click( function(){
+    $(".member").hide();
+    $("#quyen_info").show();
+  });
+  $("#lily").click( function(){
+    $(".member").hide();
+    $("#lily_info").show();
+  });
+  $("#mihoko").click( function(){
+    $(".member").hide();
+    $("#mihoko_info").show();
+  });
+
+  // row 5
+  $("#samng").click( function(){
+    $(".member").hide();
+    $("#samng_info").show();
+  });
+  $("#vanessa").click( function(){
+    $(".member").hide();
+    $("#vanessa_info").show();
+  });
+  $("#chase").click( function(){
+    $(".member").hide();
+    $("#chase_info").show();
+  });
+
+  //row 6
+  $("#jillian").click( function(){
+    $(".member").hide();
+    $("#jillian_info").show();
+  });
+  $("#melissa").click( function(){
+    $(".member").hide();
+    $("#melissa_info").show();
   });
 
 });
