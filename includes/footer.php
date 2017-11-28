7d781d36613334aad244724d8d7afcbb8faa38e9
Sarah Zhang <Sarah@dhcp-mcfad-1843.redrover.cornell.edu>
2017-11-28 08:28:59 -0500

    updated spotify link

--- includes/footer.php
+++ includes/footer.php
@@ -9,7 +9,7 @@
     <li><a href="https://www.facebook.com/HearsayACappella/"> <i class="fa fa-facebook-square fa-1x"></i> </a></li>
     <li><a href="https://www.youtube.com/user/HearsayACappella"><i class="fa fa-youtube-square fa-1x"></i> </a></li>
 <div id="spotify">
-    <li><a href=""><i class="fa fa-spotify fa-1x" title="Coming out soon!"></i></a></li>
+    <li><i class="fa fa-spotify fa-1x" title="Spotify coming soon!"></i></li>
   </div>
   </ul>
 </div>
